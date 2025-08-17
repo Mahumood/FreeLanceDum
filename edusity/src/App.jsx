cc66b7d8c41995bc5fdca7d11c57fd7fa226066c
Mahumood-Sameema <sameemamahumood@gmail.com>
2025-08-17 16:42:19 +0530

    First Commit

---
+++ edusity/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Narbar from './Components/Navbar/Narbar'
+import Hero from './Components/Hero/Hero'
+
+const App = () => {
+  return (
+    <div>
+      <Narbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
